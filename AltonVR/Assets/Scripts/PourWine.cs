ce8e6d06a2663b5fe30f42bd5765f8b3f2d87a8d
markjeffcock <mark.jeffcock@gmail.com>
2022-07-12 18:42:54 +0100

    Pourwine in place, some work needed on Booleans

---
+++ AltonVR/Assets/Scripts/PourWine.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PourWine : MonoBehaviour
+{
+    public int pourThreshold = 90;
+    public GameObject wineStream;
+    public Transform wineBottle;
+    public Transform bottleTop;
+
+    private bool isPouring = false;
+    private bool pourCheck;
+    public float bottleTopHeight;
+    public float bottleMidHeight;
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+        bottleMidHeight = wineBottle.position.y;
+        bottleTopHeight = bottleTop.position.y;
+
+        if ((bottleTopHeight-bottleMidHeight) > 0)
+        {
+            pourCheck = false;
+        }
+        else
+        {
+            pourCheck = true;
+        }
+
+        if(isPouring != pourCheck)
+        {
+            isPouring = pourCheck;
+
+            if(isPouring)
+            {
+                StartPour();
+            }
+            else
+            {
+                EndPour();
+            }
+        }
+    }
+
+ 
+    private void StartPour()
+    {
+        wineStream.SetActive(true);
+    }
+
+    private void EndPour()
+    {
+        wineStream.SetActive(false);
+    }
+
+}
