a4564fd36c5ed300abf0b0d40a9519ce9374929a
markjeffcock <mark.jeffcock@gmail.com>
2022-09-20 16:44:34 +0100

    FireAxe script working

---
+++ AltonVR/Assets/Scripts/FireAxe.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireAxe : MonoBehaviour
+{
+    public GameObject fireAxePrefab;
+    
+    public void CreateFireAxe()
+    {
+        GameObject newAxe = Instantiate(fireAxePrefab);
+    }
+
+}
